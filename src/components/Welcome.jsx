ea0077b9fe1c4c61f22fa3c679e4a053f1ba098a
ywon0925 <9788667@gmail.com>
2022-09-02 11:55:47 +0900

    Front-end Half Complete

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -5,27 +5,46 @@ import { BsInfoCircle, bsInfoCircle } from 'react-icons/bs';
 import { Loader } from './';
 
 const commonStyles = "min-h-[70px] sm:px-0 px-2  flex justify-center items-center border-[0.5px] border-gray-400 text-sm font-light text-white";
-const Input = ({ placeholder, name, type, value, handleChange }) => {
-    return (<input
+const Input = ({ placeholder, name, type, value, handleChange }) => (
+    <input
         placeholder={placeholder}
         type={type}
         step="0.0001"
         value={value}
         onChange={(e)=> handleChange(e,name)}
-        className="my-2"
+        className="my-2 w-full rounded-sm p-2 outline-none bg-transparent text-white border-none text-sm white-glassmorphism"
     />
     )
-}
 
 const Welcome = () =>{
     const connectWallet = () =>{
 
     }
-    
+    const handleSubmit = () => {
+
+    }
+    const randInclusive = (min, max) => {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(Math.random() * (max - min + 1)) + min
+    }
+    const changeColor = () => {
+        let card = document.getElementById("hello")
+        for (let i = 1; i < 8; i++){
+            let n1 = Math.round(Math.random() * 100)
+            let n2 = Math.round(Math.random() * 100)
+            let h = randInclusive(0, 360);
+            let s = randInclusive(50, 100);
+            let l = randInclusive(50, 100);
+            let color =`${n1}% ${n2}%, hsla(${h}, ${s}%, ${l}%, 1)`; 
+            card.style.setProperty('--c'+i, color);
+        }
+    }
+
     return (
         <div className="flex w-full justify-center items-center">
-            <div className="flex md:flex-row flex-col items-start justify-between md:p-20 py-12 px-4">
-                <div className="flex flex-1 justify-start flex-col md:mr-10">
+            <div className="flex mf:flex-row flex-col items-start justify-between md:p-20 py-12 px-4">
+                <div className="flex flex-1 justify-start flex-col mf:mr-10">
                     <h1 className="text-3xl sm:text-5xl text-white text-gradient py-1">
                         Create <br/> Business Card NFT
                     </h1>
@@ -41,7 +60,7 @@ const Welcome = () =>{
                             Connect Wallet
                         </p>
                     </button>
-
+                    
                     <div className="grid sm:grid-cols-3 grid-cols-2 w-full mt-10">
                         <div className={`rounded-tl-2xl ${commonStyles}`}>
                             Reliablility
@@ -63,8 +82,8 @@ const Welcome = () =>{
                         </div>
                     </div>
                 </div>
-                <div className="flex flex-col flex-1 items-center justify-start w-full md:mt-0 mt-10">
-                    <div className="p-3 justify-end items-start flex-col rounded-xl h-40 sm:w-72 w-72 my-5 eth-card white-glassmorpism">
+                <div className="flex flex-col flex-1 items-center justify-start w-full mf:mt-0 mt-10">
+                    <div id="hello" className="p-3 justify-end items-start flex-col rounded-xl h-40 sm:w-72 w-72 my-5 eth-card white-glassmorpism">
                         <div className="flex justify-between flex-col w-full h-full">
                             <div className="flex justify-between item-start">
                                 <div className="w-10 h-10 rounded-full border-2 border-white flex justify-center items-center">
@@ -82,11 +101,29 @@ const Welcome = () =>{
                             </div>
                         </div>
                     </div>
+
                     <div className="p-5 sm:w-96 w-full flex flex-col justify-start items-center blue-glassmorphism">
-                        <Input placeholder="AddressTo" name="addressTo" type="text" handleChange={() => {}} />
+                        <button className="my-2 w-full p-2 outline-none bg-transparent text-white rounded-full border-none text-sm white-glassmorphism hover:bg-[#10365b] cursor-pointer" type="button" onClick={changeColor}>Change Base Card</button>
+                        <Input placeholder="Company" name="Company" type="text" handleChange={() => {}} />
+                        <Input placeholder="Job title" name="jobTitle" type="text" handleChange={() => {}} />
+                        <Input placeholder="Job title" name="jobTitle" type="text" handleChange={() => {}} />
+                        <Input placeholder="Job title" name="jobTitle" type="text" handleChange={() => {}} />
+                        <Input placeholder="Job title" name="jobTitle" type="text" handleChange={() => {}} />
+
+                        <div className="h-[1px] w-full bg-gray-400 my-2"/>
+                        {false ? (
+                            <Loader />
+                        ) : (
+                            <button
+                                type="button"
+                                onClick={handleSubmit}
+                                className="text-white w-full mt-2 border-[1px] p-2 border-[#3d4f7c] rounded-full cursor-pointer"
+                            >
+                            Create Now 
+                            </button> 
+                        )}
                     </div>
                 </div>
-
             </div>    
         </div>
     )
